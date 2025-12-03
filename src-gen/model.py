5fb5b3546bd7258d0f2b9e7c373d28694c1aa178
Yvo Hu <yvo.hu@alliander.com>
2025-12-03 21:25:52 +0100

    test

--- src-gen/model.py
+++ src-gen/model.py
@@ -1670,6 +1670,9 @@ class Model:
 			#If no transition was taken
 			if transitioned_after == transitioned_before:
 				#then execute local reactions.
+				self.grid.row = (int((((((self.odom.y - self.start_pos.zero_y)) / self.grid.grid_size) + 0.5))))
+				self.grid.column = (int((((((self.odom.x - self.start_pos.zero_x)) / self.grid.grid_size) + 0.5))))
+				self.grid.orientation = 0 if (self.imu.yaw >= -(45.0) and self.imu.yaw < 45.0) else (1 if (self.imu.yaw >= 45.0 and self.imu.yaw < 135.0) else (2 if (self.imu.yaw >= 135.0 or self.imu.yaw < -(135.0)) else 3))
 				self.__v = self.__cmd_speed
 				self.__w = self.__cmd_rot
 				self.__target_yaw = 0.0 if (self.grid.orientation == 0) else (90.0 if (self.grid.orientation == 1) else (180.0 if (self.grid.orientation == 2) else -(90.0)))
